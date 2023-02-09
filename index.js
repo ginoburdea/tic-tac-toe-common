31e8e619dfb67856faa1122604692b167888a3e9
Gino Burdea <burdea.gino@gmail.com>
2023-02-09 13:56:12 +0200

    feat: add batchesOf function

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ export const genGameData = () => {
         },
     }
 }
+
+/**
+ * @param {any[]} arr
+ * @param {number} batchLength
+ * @returns {any[][]}
+ */
+export const batchesOf = (arr, batchLength) => {
+    return arr.reduce(
+        (acc, item) => {
+            if (acc[acc.length - 1].length === batchLength) acc.push([item])
+            else acc[acc.length - 1].push(item)
+
+            return acc
+        },
+        [[]]
+    )
+}
