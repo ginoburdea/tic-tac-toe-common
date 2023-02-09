388fb5733503029cd7b66b4de4e082249b98446d
Gino Burdea <burdea.gino@gmail.com>
2023-02-09 13:49:17 +0200

    feat: add genGameData function

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ import { customAlphabet } from 'nanoid'
  * @returns {number}
  */
 export const genRandomNumber = digits => +customAlphabet('0123456789')(digits)
+
+export const genGameData = () => {
+    return {
+        roomId: genRandomNumber(8),
+        gameData: {
+            /** @type {null[]} */
+            cells: Array(9).fill(null),
+            playersCount: 0,
+            playerTurn: (genRandomNumber(1) % 2) + 1,
+            winner: null,
+            /** @type {[]} */
+            winningCells: [],
+            /** @type {[]} */
+            players: [],
+            winningType: null,
+            gameStatus: 'waiting-for-opponent',
+            playerRestarting: null,
+        },
+    }
+}
